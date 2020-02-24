a59f8bf47bfc3c00359e0724df3274a63cf342bb
Alex Vitorino <halexsrv@gmail.com>
2020-02-24 19:53:09 -0300

    add setup for react navigation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  console.tron.log('test');
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Test!</Text>
-    </View>
-  );
+  return <Routes />;
 }
