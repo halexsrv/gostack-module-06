07f0a17f05c788b87ab2935b95b65cbb55b344bf
Alex Vitorino <halexsrv@gmail.com>
2020-02-24 19:24:10 -0300

    add setup for reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,6 +15,8 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  console.tron.log('test');
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Test!</Text>
